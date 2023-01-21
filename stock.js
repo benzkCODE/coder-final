9df17189031703920311a1a1e4a401b01d2f36a1
unknown <correabenjamin720@gmail.com>
2023-01-21 19:35:25 -0300

    primer commit

---
+++ stock.js
@@ -0,0 +1,12 @@
+let stockProductos = [
+    {id: 1, nombre: "PIERNA ENTERA", tipo: "buzo", cantidad: 1, desc: "Depilación piernas completa", precio: 6190,  img: './img/PIERNAENTERA.jpg'},
+    {id: 2, nombre: "MEDIA PIERNA", tipo: "buzo", cantidad: 1, desc: "Depilación media pierna", precio: 3940,  img: './img/MEDIAPIERNA.jpg'},
+    {id: 3, nombre: "ROSTRO COMPLETO", tipo: "buzo", cantidad: 1, desc: "Depilación rostro completo", precio: 3100,  img: './img/ROSTRO.jpg'},
+    {id: 4, nombre: "AXILAS", tipo: "buzo", cantidad: 1, desc: "Depilación axilas", precio: 1460,  img: './img/AXILAS.jpg'},
+    {id: 5, nombre: "GENITALES", tipo: "buzo", cantidad: 1, desc: "Depilación genitales", precio: 1200,  img: './img/GENITALES.jpg'},
+    {id: 6, nombre: "ABDOMEN", tipo: "buzo", cantidad: 1, desc: "Depilación abdomen", precio: 1700,  img: './img/ABDOMEN.jpg'},
+    {id: 7, nombre: "BARBA", tipo: "buzo", cantidad: 1, desc: "Depilación barba", precio: 2100,  img: './img/BARBA.jpg'},
+    {id: 8, nombre: "BIGOTE", tipo: "buzo", cantidad: 1, desc: "Depilación bigote", precio: 1100,  img: './img/BIGOTE.jpg'},
+    {id: 9, nombre: "ENTRECEJO", tipo: "buzo", cantidad: 1, desc: "Depilación entrecejo", precio: 900,  img: './img/ENTRECEJO.jpg'},
+    
+]
